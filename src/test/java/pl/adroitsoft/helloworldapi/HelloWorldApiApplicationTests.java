04a139d999c85b9e2f6ce173fb9d788b7c5f38ea
Marcin Szupke <marcin.szupke@gmail.com>
2022-07-08 15:28:45 +0200

    failed assertion

--- src/test/java/pl/adroitsoft/helloworldapi/HelloWorldApiApplicationTests.java
+++ src/test/java/pl/adroitsoft/helloworldapi/HelloWorldApiApplicationTests.java
@@ -2,12 +2,14 @@ package pl.adroitsoft.helloworldapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class HelloWorldApiApplicationTests {
 
     @Test
     void contextLoads() {
+        Assert.isTrue(false, "failed assertion");
     }
 
 }
